0da5896c17745a0b955b2fd6f985bb3887bd3232
Rasmi Elasmar <rrelasmar@gmail.com>
2016-12-25 01:02:41 -0500

    Using improved resizing method.

--- utils/prepare_images.py
+++ utils/prepare_images.py
@@ -1,6 +1,7 @@
 from __future__ import division
 
 import os
+import math
 import glob
 from PIL import Image
 
@@ -20,21 +21,34 @@ def resize(image, size=256):
     if smaller_side < size:
         os.remove(image)
 
-    # Calculate how much to trim to get a square image.
+    # Calculate how much scaling to get to a properly sized image on the smaller side.
     scaling_factor = smaller_side_res / size
-    new_large_side_res = larger_side_res / scaling_factor
-    crop_amount = larger_side_res - size
-    trim = int((crop_amount/2)/scaling_factor)
-    print trim
+    new_large_side_res = int(larger_side_res / scaling_factor)
+
+    if larger_side:
+        # Height is larger, scale limit horizontally.
+        im = im.resize((size, new_large_side_res))
+    else:
+        # Height is larger, scale limit vertically.
+        im = im.resize((new_large_side_res, size))
+
+    # Trim the rest to reach size on the larger end
+    crop_amount = new_large_side_res - size
+    if crop_amount % 2 == 0:
+        pad = 0
+    else:
+        pad = 1
+    trim = math.floor(crop_amount/2)
+
     if larger_side:
         # Height is larger, trim vertically.
-        box = (0, 0 + trim, smaller_side_res, larger_side_res - trim)
+        box = (0, 0 + trim + pad, size, new_large_side_res - trim)
     else:
         # Width is larger, trim horizontally.
-        box = (0 + trim, 0, larger_side_res - trim, smaller_side_res)
+        box = (0 + trim + pad, 0, new_large_side_res - trim, size)
 
     print box
-    # Resize and crop.
+    # Crop.
     im = im.crop(box)
-    im = im.resize((size, size))
+    
     im.save(image, 'JPEG')
